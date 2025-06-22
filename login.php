d26c986b1c2a257c7d0d2d2aca926c4165fe7f46
Mangesh Pandit <pandit.mangesh002@gmail.com>
2025-06-22 22:08:38 +0530

    -Php

--- login.php
+++ login.php
@@ -1,6 +1,8 @@
 <?php
+
+if($_SERVER ["REQUEST_METHOD"]=="GET"){
 echo "User name is ".$_GET['user_name']; 
 echo "<br/>";
 echo "User Password is ".$_GET['password'];
-
+}
 // login form is just for know the interaction with html and php 
