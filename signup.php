db24281dda17d3f48f7e7978a45837754f1e0a4a
Mangesh Pandit <pandit.mangesh002@gmail.com>
2025-06-23 11:31:54 +0530

    Update signup.php

--- signup.php
+++ signup.php
@@ -2,4 +2,8 @@
 echo print_r($_REQUEST);
 // we can use $_REQUEST to get all the data from the form
 // we can also use $_POST or $_GET depending on the method used in the form
+
+foreach ($_REQUEST as $key => $value) {
+    echo "$key: $value<br>";
+}
 ?>
