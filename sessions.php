6a55c63ab2b0777d08fcbfc5658bf49b5138cf64
Mangesh Pandit <pandit.mangesh002@gmail.com>
2025-06-25 13:10:53 +0530

    Create sessions.php

---
+++ sessions.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Session Page</title>
+</head>
+<body>
+    <form action="" method="post">
+        <input type="text" name="user" placeholder="Enter your name" />
+        <br><br>
+        <button name="button" value="set">Set Sessions</button>
+        <br><br>
+        <button name="button" value="get"> Display Sessions</button>
+        <br><br>
+        <button name="button" value="delete"> Delete Session</button>
+    </form>
+</body>
+</html>
+
+<?php
+session_start();
+
+if(isset($_POST['button'])){
+    if($_POST['button'] =="set"){
+        $val=$_POST['user'];
+        $_SESSION['user'] = $val; // Set a session variable named "user" with input value
+
+    }
+
+    if($_POST['button']=="get"){
+       echo $_SESSION['user']; // Display value of the "user" session variable
+    }
+
+    if($_POST['button']=="delete"){
+        session_destroy();
+    }
+}
